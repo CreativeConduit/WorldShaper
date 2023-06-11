d5b11a41e2c78252e77106485495b788405e4cd1
CodedStingray <ray.kampmann@gmail.com>
2023-06-11 19:35:13 +0200

    Setup WorldShaper plugin

---
+++ src/main/java/net/codedstingray/worldshaper/WorldShaper.java
@@ -0,0 +1,13 @@
+package net.codedstingray.worldshaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldShaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+        getLogger().info("WorldShaper successfully initialized");
+    }
+}
